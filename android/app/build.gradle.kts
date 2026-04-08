e5693693f8cada8de317a5a0e0205de068b7c0b2
Kaisar Iskak <kaisariskak@gmail.com>
2026-04-08 15:51:56 +0600

    error for android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,3 +42,9 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+}
